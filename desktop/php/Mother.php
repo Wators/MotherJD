1f378e389b6df26e43b0367bbeca0f0b82fadff1
Wators <gladysmaisonauto@gmail.com>
2015-07-12 09:59:17 +0200

    V0.1

--- desktop/php/Mother.php
+++ desktop/php/Mother.php
@@ -9,7 +9,7 @@ sendVarToJS('eqType', 'Mother');
     <div class="col-lg-2">
         <div class="bs-sidebar affix">
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav fixnav">
-                <li class="nav-header">Liste équipements Mother
+                <li class="nav-header">Liste des cookies Mother 
                     <i class="fa fa-plus-circle pull-right cursor eqLogicAction" data-action="add" style="font-size: 1.5em;margin-bottom: 5px;"></i>
                 </li>
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="Rechercher" style="width: 100%"/></li>
@@ -27,36 +27,10 @@ sendVarToJS('eqType', 'Mother');
             <fieldset>
                 <legend>Général</legend>
                 <div class="form-group">
-                    <label class="col-lg-3 control-label">Nom de l'équipement Mother</label>
+                    <label class="col-lg-3 control-label">Nom de l'équipement</label>
                     <div class="col-lg-3">
                         <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
                         <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="Nom de l'équipement Mother"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-3 control-label" >Objet parent</label>
-                    <div class="col-lg-3">
-                        <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                            <option value="">Aucun</option>
-                            <?php
-                            foreach (object::all() as $object) {
-                                echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-3 control-label">Catégorie</label>
-                    <div class="col-lg-8">
-                        <?php
-                        foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-                            echo '<label class="checkbox-inline">';
-                            echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-                            echo '</label>';
-                        }
-                        ?>
-
                     </div>
                 </div>
                 <div class="form-group">
@@ -64,58 +38,17 @@ sendVarToJS('eqType', 'Mother');
                     <div class="col-lg-1">
                         <input type="checkbox" class="eqLogicAttr form-control" data-l1key="isEnable" checked/>
                     </div>
-                     <label class="col-lg-3 control-label">Visible</label>
-                    <div class="col-lg-1">
-                        <input type="checkbox" class="eqLogicAttr form-control" data-l1key="isVisible" checked/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-3 control-label">Code</label>
-                    <div class="col-lg-1">
-                        <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="code" checked/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-3 control-label">Nom de la commande</label>
-                    <div class="col-lg-3">
-                        <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="cmd_name" />
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-3 control-label">Sous-type de la commande</label>
-                    <div class="col-lg-3">
-                        <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="cmd_subtype" >
-                            <?php
-                            foreach (jeedom::getConfiguration('cmd:type:info:subtype') as $key => $value) {
-                                echo '<option value="' . $key . '" >' . $value['name'] . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-3 control-label">Unité de la commande</label>
-                    <div class="col-lg-3">
-                        <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="cmd_unite" />
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-3 control-label">Afficher la commande</label>
-                    <div class="col-lg-1">
-                        <input type="checkbox" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="cmd_isVisible" checked/>
-                    </div>
                 </div>
             </fieldset> 
         </form>
 
-        <legend>Commande</legend>
-
-
-        <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> Ajouter une commande</a><br/><br/>
+        <legend>Mother</legend>
+        <div class="alert alert-info">L'URL de l'agenda google se trouve dans Paramètres>Agenda>[Agenda voulu]>Adresse privée XML</div>
+        <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> Ajouter une commande google agenda</a><br/><br/>
         <table id="table_cmd" class="table table-bordered table-condensed">
             <thead>
                 <tr>
-                    <th>Nom</th><th>Valeur</th><th></th>
+                    <th>Nom</th><th>URL de l'agenda</th><th></th>
                 </tr>
             </thead>
             <tbody>
@@ -131,9 +64,8 @@ sendVarToJS('eqType', 'Mother');
                 </div>
             </fieldset>
         </form>
-
     </div>
 </div>
 
-<?php include_file('desktop', 'Mother', 'js', 'Mother'); ?>
+<?php include_file('desktop', 'Mother', 'js', 'Mother); ?>
 <?php include_file('core', 'plugin.template', 'js'); ?>
